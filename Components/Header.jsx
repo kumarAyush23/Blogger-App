2dd35b05547c881caeac7c9799a3f7ba9cd1abae
Ayush Kumar <102895174+kumarAyush23@users.noreply.github.com>
2024-08-06 19:35:07 +0530

    Init

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
       <div className='flex justify-between items-center'>
-        <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
+        <Image src={assets.logo} alt='' width={180}  className='w-[130px] sm:w-auto'/>
         <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get started <Image src={assets.arrow} /></button>
       </div>
       <div className='text-center my-8'>
